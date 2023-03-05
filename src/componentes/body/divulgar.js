ff9279befd976d63b478380aff57aeb3b7db784c
VitorVitima <yukkiegg@gmail.com>
2023-03-05 14:11:47 -0300

    save

--- src/componentes/body/divulgar.js
+++ src/componentes/body/divulgar.js
@@ -1,23 +1,47 @@
 import './divulgar.css'
 import { useState, useEffect } from 'react';
 function Divulgar(){
-    const [key, setKey] = useState()
+    const [keyApi, setKeyApi] = useState()
     const [keyClient, setKeyClient] = useState()
     useEffect(()=>{
         fetch('https://chavesDF.ai-se-fosse-o-p.repl.co')
         .then((api)=>api.json())
-        .then((api)=>setKey(api))
+        .then((api)=>setKeyApi(api))
     })
+    function subButton(e){
+        e.target.addeventlistener('submit', ()=>{
+            const formData = new FormData(e)
+            const data = Object.fromEntries(formData)
+
+            fetch('https://DivulgaFone.ai-se-fosse-o-p.repl.co', {
+                method: 'POST',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(data)
+            }).then(res => res.json()).then(data=>console.log(data))
+        })
+    }
+    function enviarDados(tag){
+        const key = document.querySelector('#keyInput')
+        keyApi?.map((e)=>{
+            if(e.chave == key.value){
+                subButton(tag)
+            } else{
+                window.alert('Chave de Divulgação inválida...')
+            }
+        })
+    }
     return(
-        <section id='sForm'>
-            <form autoComplete={'off'}>
+        <section id='sForm' action="https://DivulgaFone.ai-se-fosse-o-p.repl.co">
+            <form autoComplete={'off'} id='form-api'>
                 <div id='nome'>
                    <label htmlFor='nomeEs' >Nome da empresa</label>
                    <input id='nomeEs' type={'text'} maxLength='20' minLength='1' required/>
                 </div>
                 <div id='telefone'>
                     <label htmlFor='teleEs'>Telefone de contato</label>
-                    <input id='teleEs' type={'number'} min='10' max='11' required/>
+                    <input id='teleEs' type={'number'} minLength='1' required/>
                 </div>
                 <div id='endereco'>
                     <label htmlFor='endeEs'>Endereço</label>
@@ -30,7 +54,7 @@ function Divulgar(){
                 </div>
                 <div id='cep'>
                     <label htmlFor='cepEs'>Cep</label>
-                    <input id='cepEs' type={'number'} min='8' max='8' required/>
+                    <input id='cepEs' type={'number'} minLength='8' maxLength={'8'} required/>
                 </div>
                 <div id='categoria'>
                     <label htmlFor='categoriasEscolha'>Categoria</label>
@@ -56,7 +80,7 @@ function Divulgar(){
                     <input id='keyInput' type={'text'}/>
                 </div>
                 <div id='button'>
-                    <input type={'submit'} value='Divulgar'/>
+                    <input onClick={(e)=>enviarDados(e)} type={'submit'} value='Divulgar'/>
                 </div>
             </form>
         </section>
