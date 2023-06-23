218e973f537449d7febb1737a56db37f31f80e95
Vitor Vitima <90335478+VitorVitima@users.noreply.github.com>
2023-06-23 10:56:31 -0300

    Update globais.js

--- src/componentes/globais.js
+++ src/componentes/globais.js
@@ -3,5 +3,5 @@ export default class Globais{
     static titu = 'Parceiros'
     //https://divulga-back.vercel.app
     //https://divulga-back2.onrender.com
-    static urlBack = 'https://chavesdf.ai-se-fosse-o-p.repl.co'
+    static urlBack = 'https://divulgafone.ai-se-fosse-o-p.repl.co'
 }
